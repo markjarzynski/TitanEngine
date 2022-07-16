56932ee1061fe8e3c7bbd8f19cd4d70c1a435180
Mark Jarzynski <mark.jarzynski@gmail.com>
2022-07-16 01:10:31 -0400

    Initial commit.

---
+++ NewEngine.cpp
@@ -0,0 +1,10 @@
+#include "NewEngineConfig.h"
+
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+    std::cout << NewEngine_PROJECT_NAME << " " << NewEngine_VERSION << std::endl;
+
+    return 0;
+}
