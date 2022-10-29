b4b906272ec71e0a051fae7f997dbcbd28606a00
Mark Jarzynski <mark.jarzynski@gmail.com>
2022-10-29 15:01:01 -0400

    Beginning work on suppoting metal.

---
+++ Graphics/Metal.cpp
@@ -0,0 +1,9 @@
+#include "Metal.h"
+
+namespace Titan
+{
+namespace Graphics
+{
+
+}; // namespace Graphics
+}; // namespace Titan
---
+++ Graphics/Metal.h
@@ -0,0 +1,10 @@
+namespace Titan
+{
+namespace Graphics
+{
+class Metal
+{
+
+};
+}; // namespace Graphics
+}; // namespace Titan
